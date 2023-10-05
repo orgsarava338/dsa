8b07467a331662635df948527b46bea173e3df50
sarava338 <sarava338@gmail.com>
2023-10-05 23:50:21 +0530

    test cases are added for linked lists

---
+++ src/test/LinkedList.test.ts
@@ -0,0 +1,27 @@
+import { beforeEach, describe, expect, test } from "bun:test";
+import LinkedList from "../linked-list/LinkedList";
+
+describe("Linked Lists Tests", () => {
+  let list: LinkedList<number>;
+  const values = [1, 2, 3, 4, 5];
+
+  beforeEach(() => {
+    list = new LinkedList(values[0]);
+  });
+
+  test("create a list", () => {
+    expect(list.toArray()).toEqual([values[0]]);
+  });
+
+  test("append a value", () => {
+    list.append(2);
+    expect(list.toArray()).toEqual([values[0], values[1]]);
+  });
+
+  test("delete a value", () => {
+    list.delete(values[0]);
+    expect(list.toArray()).toEqual([]);
+  });
+
+  test("list is empty", () => {});
+});
