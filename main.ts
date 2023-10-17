aa84c7a429c6ac0c271888f3933e124d536f1e53
sarava338 <sarava338@gmail.com>
2023-10-17 23:52:06 +0530

    corrected the wrong import

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import Stack from "./src/stack";
-import Queue from "./src/queue/Queue";
+import Queue from "./src/queue/";
 import SinglyLinkedList from "./src/list/singly-linked-list/linear";
 import DoublyLinkedList from "./src/list/doubly-linked-list/linear";
 import CircularSinglyLinkedList from "./src/list/singly-linked-list/circular";
