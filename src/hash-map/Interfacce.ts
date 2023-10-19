617fd5e5d696001d5f3861ac23c5660702857b24
sarava338 <sarava338@gmail.com>
2023-10-19 22:07:43 +0530

    HashMap methods were added

--- src/hash-map/Interfacce.ts
+++ src/hash-map/Interfacce.ts
@@ -1,3 +1,14 @@
-export interface IHashMap {
+export interface IHashMap<K, V> {
+  readonly elements: V[];
+
   isInstanceOf(classToCheck: { new (): any }): Boolean;
+  hash(ket: K): number;
+  put(ket: K, value: V): void;
+  get(key: K): V | undefined;
+  has(key: K): Boolean;
+  remove(key: K): void;
+  keys(): K[];
+  values(): V[];
+  size(): number;
+  clear(): void;
 }
--- src/hash-map/index.ts
+++ src/hash-map/index.ts
@@ -1,7 +1,44 @@
 import { IHashMap } from "./Interfacce";
 
-export default class HashMap implements IHashMap {
+export default class HashMap<K, V> implements IHashMap<K, V> {
+  readonly elements: V[];
+
+  constructor() {
+    this.elements = [];
+  }
+
   isInstanceOf(classToCheck: new () => any): Boolean {
     return this instanceof classToCheck;
+  }
+
+  hash(ket: K): number {
+    throw new Error("Method not implemented.");
+  }
+
+  put(ket: K, value: V): void {
+    throw new Error("Method not implemented.");
+  }
+
+  get(key: K): V | undefined {
+    throw new Error("Method not implemented.");
+  }
+
+  has(key: K): Boolean {
+    throw new Error("Method not implemented.");
+  }
+  remove(key: K): void {
+    throw new Error("Method not implemented.");
+  }
+  keys(): K[] {
+    throw new Error("Method not implemented.");
+  }
+  values(): V[] {
+    throw new Error("Method not implemented.");
+  }
+  size(): number {
+    throw new Error("Method not implemented.");
+  }
+  clear(): void {
+    throw new Error("Method not implemented.");
   }
 }
