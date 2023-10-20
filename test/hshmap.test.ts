75f8806766c266bc7fc27ba16f0af49c8b7d4191
sarava338 <sarava338@gmail.com>
2023-10-20 18:00:34 +0530

    map test modified

--- test/hshmap.test.ts
+++ test/hshmap.test.ts
@@ -1,8 +1,11 @@
 import { beforeEach, describe, expect, test } from "bun:test";
 import HashMap from "../src/hash-map";
 
+type K = any;
+type V = any;
+
 describe("HashMap Tests", () => {
-  let map: HashMap;
+  let map: HashMap<K, V>;
 
   beforeEach(() => {
     map = new HashMap();
